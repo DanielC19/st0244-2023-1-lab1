e9b47bbc5b34653e6d7b14046b5594646fd2b366
juanrobles <116527097+juanrobles05@users.noreply.github.com>
2023-04-09 19:02:09 -0500

    Feature: Calculate mean in C++
    - Add C++ file which receives an array from Java and return its mean

---
+++ src/Mean.cpp
@@ -0,0 +1,24 @@
+#include <C:\Program Files\Java\jdk-19\include\jni.h>
+#include <iostream>
+#include "Mean.h"
+using namespace std;
+
+JNIEXPORT jdouble JNICALL Java_Mean_mean
+    (JNIEnv *env, jobject obj, jintArray arr) {
+    // The elements of the array are obtained
+    jint *cArray = (*env).GetIntArrayElements(arr, NULL);
+    // It is checked that the array is not empty and if it is empty it returns 0.0
+    if (cArray == NULL) return 0.0;
+    if (obj == NULL) return 0.0;
+    // The size of the array is saved
+    jsize length = (*env).GetArrayLength(arr);
+    
+    jint sum = 0;
+    int i;
+    for (i = 0; i < length; i++) {
+        sum += cArray[i];
+    }
+    jdouble mean = (jdouble)sum / length;
+    
+    return mean;
+}
